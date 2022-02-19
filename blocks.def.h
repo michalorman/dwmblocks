7343fb06226dcabccecc338ffb75d199fae2f4b5
Michal Orman <michal.orman@gmail.com>
2022-02-19 21:27:13 +0100

    defined blocks

--- blocks.def.h
+++ blocks.def.h
@@ -1,11 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+  /*icon    commnd          interval  signal */
+	{"",      "sb-volume",	  0,		    21}, // kill -55 $(pidof dwmblocks) # add 34 to signal
+	{"",      "sb-memory",	  5,		    22},
+  {"",      "sb-datetime",	30,		    23},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
+static char delim[] = "  ";
 static unsigned int delimLen = 5;
